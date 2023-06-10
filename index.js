ceb77ef7299a316a99df0fddfb7bc336166c781c
USPapon <papon.fsp@gmail.com>
2023-06-10 17:18:02 +0600

    Ourclass and Our Instructor related API routes has been implemented

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
+/******
+ * axiosSecure or fetch ('url', {method: '', headers: {authorization: ''Barer token}})
+ * sends authorization with the document header
+ * the bellow function reads that header with line number #21
+ * if authorization not found it do not let the route(url) to be hit
+ * **/ 
 const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization; // check if reques has authorization in the header
     if (!authorization) {
@@ -30,9 +35,6 @@ const verifyJWT = (req, res, next) => {
         console.log("jwt found this: ", req.decoded);
         next();
     })
-
-
-
 }
 
 
@@ -52,11 +54,19 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const userCollection = client.db("mappifyMindDB").collection("users");
+        const classCollection = client.db("mappifyMindDB").collection("classes");
 
         // API to check authorization
+
+        /****
+         * work jointly with onAuthstatechange and set toke in .env file and always 
+         * monitors if user came with a valid accesstoken that matches with the token
+         * stored in .env file
+         * or not. 
+         * **/ 
         app.post('/jwt', (req, res) => {
             const user = req.body;
             console.log("user", user);
@@ -64,6 +74,10 @@ async function run() {
             res.send({ token });
         })
 
+        /****
+         * prevents accessing this route from accessing the routes(url)
+         * from those who are already system user but role wise forbidden
+         * **/ 
         const verifyAdmin = async (req, res, next) => {
             const email = req.decoded.email;
             // console.log("decoded=", req.decoded.email, "past=", req.decoded.email)
@@ -87,27 +101,10 @@ async function run() {
             next();
         }
 
-        // USER related APIs
-        // app.get('/users/admin/:email', verifyJWT, async(req, res) => {
-        //     const email = req.params.email;
-            
-        //     console.log('i am hit')
-        //     console.log(email);
-
-        //     if(req.decoded.email !== email){
-        //         return res.send({admin: false})
-        //     }
-
-        //     const query = { email: email};
-        //     const foundUser = await userCollection.findOne(query);
-            
-        //     console.log(foundUser);
-        //     const result = { admin: foundUser?.role === 'admin' }
-        //     res.send(result);
-        // })
-
-
-
+        /***
+         * used for condetional rendering of content according 
+         * to the user role
+         * **/  
         app.get('/user/role/:email', verifyJWT, async(req, res) => {
             const email = req.params.email;
 
@@ -173,6 +170,72 @@ async function run() {
             res.send(result);
         })
 
+        // ADMIN DASHBOARD
+        // Class related APIs   
+        app.get('/admin/allclasses/', async(req, res) => {
+            const result = await classCollection.find().toArray();
+            console.log(result);
+            res.send(result);
+        })   
+        
+        app.patch('/admin/class/approve/:id', async (req, res) => {
+            const id = req.params;
+            console.log(id)
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: { status: 'approved' }
+            }
+            const result = await classCollection.updateOne(filter, updatedDoc);
+            console.log(result);
+            res.send(result);
+        })
+
+        app.patch('/admin/class/deny/:id', async (req, res) => {
+            const id = req.params;
+            console.log(id)
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: { status: 'denied' }
+            }
+            const result = await classCollection.updateOne(filter, updatedDoc);
+            console.log(result);
+            res.send(result);
+        })        
+
+        // INSTRUCTOR DASHBOARD
+        // Class related APIs
+        app.post('/class', verifyJWT, verifyInstructor, async (req, res) => {
+            const newClass = req.body;
+            console.log(newClass);
+            const result = await classCollection.insertOne(newClass);
+            console.log(result)
+            res.send(result);
+          })
+
+        app.get('/instructor/myclasses/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = {email: email};
+            const result = await classCollection.find(query).toArray();
+            console.log(result);
+            res.send(result);
+        })
+
+
+
+
+        // API Related to website
+        app.get('/ourclasses', async(req, res) => {
+            const query = {status: 'approved'};
+            const result = await classCollection.find(query).toArray();
+            console.log(result);
+            res.send(result);
+        })
+        app.get('/ourinstructors', async(req, res) => {
+            const query = {role: 'instructor'};
+            const result = await userCollection.find(query).toArray();
+            console.log(result);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
