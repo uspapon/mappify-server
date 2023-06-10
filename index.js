c15bc55ccd60eada47a7ac75bc8a6f64404c3263
USPapon <papon.fsp@gmail.com>
2023-06-10 23:30:15 +0600

    vercel deployment realated code has been added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
+require('dotenv').config();
 
 
 const port = process.env.PORT || 5000;
