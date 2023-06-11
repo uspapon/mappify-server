c117b6981420fd0f257397fb439ec9730335eaae
USPapon <papon.fsp@gmail.com>
2023-06-11 09:26:18 +0600

    enrolled-classes route has been implemented

--- index.js
+++ index.js
@@ -289,7 +289,14 @@ async function run() {
 
         app.get('/bookings/:email', async (req, res) => {
             const email = req.params.email;
-            const query = { email: email };
+            const query = { email: email, status: 'pending' };
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result); 
+        })
+
+        app.get('/enrolled-classes/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email, status: 'paid' };
             const result = await bookingCollection.find(query).toArray();
             res.send(result); 
         })
